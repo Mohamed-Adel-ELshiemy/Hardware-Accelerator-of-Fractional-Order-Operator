5fbbc01352bbfb1efad7a98525f7ad16992b19fb
Mohamed Adel Elshiemy <156431969+Mohamed-Adel-ELshiemy@users.noreply.github.com>
2024-09-17 14:58:36 +0300

    Update Deform_Frac.v

--- Deform_Frac.v
+++ Deform_Frac.v
@@ -2,7 +2,7 @@
 
 module Deform_Frac(
 
-input clk, rst,
+input clk, Rst_n,
 input  [31:0] Signal,
 output reg [31:0] Output,
 output reg OutInd
@@ -11,8 +11,8 @@ output reg OutInd
 reg signed [63:0] temp;
 reg signed [31:0] temp1;
 reg signed [31:0] temp2;
-reg signed [31:0] PrevSig;
-integer StepBeta = 16861102;
+reg signed [31:0] Prev_Sig;
+integer Step_Beta = 16861102;
 integer alpha = 8388608; // .5 *2^24
 integer step =   167770; // .01 *2^24
 integer beta = 8388608; // (1- alpha)
@@ -20,13 +20,13 @@ integer beta = 8388608; // (1- alpha)
 
 initial 
 begin
-PrevSig = 0;
+Prev_Sig = 0;
 OutInd = 0;
 
 end
 always @ (posedge clk)
 begin
-    if (!rst)
+    if (!Rst_n)
     begin
 //        temp = (2^24)*beta;
 //        temp1 = temp[55:24];
@@ -34,12 +34,12 @@ begin
         temp = temp2 *alpha;
         temp2 = temp[55:24];
         temp2 = temp2 + Signal;
-        temp = temp2*StepBeta;
+        temp = temp2*Step_Beta;
         temp1 = temp[55:24];
         temp2 = temp1-Signal;
         temp = temp2*100;
         Output = temp[31:0];
-        PrevSig = Signal;
+        Prev_Sig = Signal;
 //        temp = temp1<<24;
 //        Output = temp/step;      
         OutInd = ~OutInd;  
